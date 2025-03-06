5720dc8e5e34cf84cf4c39aff27b04090b2ac442
Jacky Zhao <j.zhao2k19@gmail.com>
2025-03-05 17:13:19 -0800

    fix(og-image): overflow ellipses in title and description

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     baseUrl: "athena.wingadium.space",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
