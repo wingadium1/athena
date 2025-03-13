1be276896d27e697d7239e7c9ae07d0f73540261
Jacky Zhao <j.zhao2k19@gmail.com>
2025-03-13 10:41:50 -0700

    feat(og): add reading time to default, improve logging

--- quartz.config.ts
+++ quartz.config.ts
@@ -86,6 +86,7 @@ const config: QuartzConfig = {
       Plugin.Assets(),
       Plugin.Static(),
       Plugin.NotFoundPage(),
+      // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
     ],
   },
