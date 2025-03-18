23f2baa0c2daee391abe6d098e54aaef95bd44f8
Jacky Zhao <j.zhao2k19@gmail.com>
2025-03-18 08:56:06 -0700

    fix(git): deprioritize git, dont fail on non-git content folders

--- quartz.config.ts
+++ quartz.config.ts
@@ -56,7 +56,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["git", "frontmatter", "filesystem"],
+        priority: ["frontmatter", "git", "filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
