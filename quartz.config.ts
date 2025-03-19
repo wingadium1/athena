fd365d52b674f8ab3faaf41621ef536ca9759019
Shane McDonald <me@shanemcd.com>
2025-03-19 15:47:16 -0400

    fix(lastmod) Change defaultDateType to "modified" (#1862)
    Making this change as per https://github.com/jackyzha0/quartz/issues/1857#issuecomment-2737098252
    
    This is necessary for the `git` source to work properly in the CreatedModifiedDate plugin.

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "athena.wingadium.space",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
